8579312d596277711aa429760e2a8457278ed3f1
Hack Reactor Students <grimi94@gmail.com>
2014-06-03 20:59:13 -0700

    Implement difference

--- src/underbar.js
+++ src/underbar.js
@@ -455,7 +455,12 @@ var _ = {};
   // Take the difference between one array and a number of other arrays.
   // Only the elements present in just the first array will remain.
   _.difference = function(array) {
-
+    var args = Array.prototype.slice.call(arguments, 1);
+    return _.filter(array, function(item) {
+      return _.every(args, function(arr) {
+        return (arr.indexOf(item) === -1);
+      });
+    });
   };
 
 
